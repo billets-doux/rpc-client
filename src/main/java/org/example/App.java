862f8701c8266247ad21ca3073ab10fdada9c5ba
billetsdoux <billetsdoux@github.com>
2020-03-21 16:03:50 +0800

    rpc-client第二版引入spring来管理

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,10 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        RpcProxyClient rpcProxyClient = new RpcProxyClient();
 
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        RpcProxyClient rpcProxyClient = context.getBean(RpcProxyClient.class);
         IHelloService iHelloService = rpcProxyClient.clientProxy(IHelloService.class, "localhost", 8080);
         String  result = iHelloService.sayHello("Billetsdoux");
         System.out.println(result);
+
     }
 }
